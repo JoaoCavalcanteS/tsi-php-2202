2045760a086c8b3d5cb772385b55bf76e7297691
João Cavalcante <joaovitorcavalcante2012@gmail.com>
2022-08-24 21:53:44 -0300

    test

--- index.php
+++ index.php
@@ -1,7 +1,4 @@
 <?php
-$nome='Aluno, João';
-
-echo "Olá $nome";
 //Comentário de linha
 #Comentário de linha 
 /*
@@ -9,4 +6,36 @@ Comentário
 de 
 bloco
 */
+$nome='Aluno, João';
+echo "Olá, $nome"; //INTERPOLAÇÃO
+//FOR//
+for ($i = 1; $i <= 10; $i++){
+    echo "<br>JOAO";
+}
+
+//WHILE;
+$i=0;
+while($i<=10){
+    echo "<br>João";
+    $i++;
+}
+
+//DO WHILE
+$c=0;
+do{
+    echo "Joaozinho";
+    $c++;
+}while($c<=10);
+//////////////////////////////////////////
+//CONDICIONAIS
+
+
+//IF
+if(11>10){
+    echo "<br>CINCO É MAIOR DO QUE DEZ";
+}
+else{
+    echo "CINCO NÃO É MAIOR DO QUE 10";
+}
+//FIM CONDICIONAIS
 ?>
