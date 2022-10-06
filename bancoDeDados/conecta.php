dccfc1eb6516c8ad223aeff3877ad246b83e0df8
João Cavalcante <joaovitorcavalcante2012@gmail.com>
2022-10-05 22:14:08 -0300

    Conectando e substituindo no SGBD

--- bancoDeDados/conecta.php
+++ bancoDeDados/conecta.php
@@ -1,5 +1,6 @@
 <?php
 //dados para nos conectarmos ao SGBD
+//Espera o tipo de BD/Qual tabela, Qual maquina(a local) e a porta dele
 $dsn= 'mysql:dbname=senac;host=localhost;port=3307';
 $user='root';
 $pass='';
@@ -7,4 +8,3 @@ $pass='';
 //conectamos ao SGBD
 $bd=new PDO($dsn, $user, $pass);
 
-var_dump($bd);
